039948bc9579f270d431d303d91d6524f63066b8
Thomas <tricki@users.noreply.github.com>
2016-11-24 17:59:51 +0100

    Fix typo

--- src/components/thumbnail/thumbnail.ts
+++ src/components/thumbnail/thumbnail.ts
@@ -5,7 +5,7 @@ import { Directive } from '@angular/core';
   * @module ionic
   * @description
   * A Thumbnail is a component that creates a squared image for an item.
-  * Thumbnails can be place on the left or right side of an item with the `item-left` or `item-right` directive.
+  * Thumbnails can be placed on the left or right side of an item with the `item-left` or `item-right` directive.
   * @see {@link /docs/v2/components/#thumbnail-list Thumbnail Component Docs}
  */
 @Directive({
